b9928c0ff53754cb88ab75d17e709e747e25ac07
HelenaMadrid <helena.madrid@directrips.com>
2019-05-06 17:48:03 +0200

    finished coding spotify-this-song

--- liri.js
+++ liri.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 var axios = require("axios");
 
 var moment = require("moment");
+var Spotify = require("node-spotify-api");
+var dotenv = require("dotenv");
 
 //js
 var keys = require("./keys");
 
 //js
-//var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
 var userCommand = process.argv[2];
 switch (userCommand) {
@@ -33,14 +35,42 @@ switch (userCommand) {
                     console.log("---------------------");
                 }
 
-            })
-
-
-        //"https://rest.bandsintown.com/artists/"+artist+"/events?app_id=codingbootcamp"
+            });
         break;
 
     case "spotify-this-song":
         console.log(userCommand);
+        var song = process.argv.slice(3).join(" ");
+        spotify
+            .search({ type: 'track', query: song })
+            .then(function (response) {
+                //console.log(response);
+                //console.log(response.tracks.items.length);
+                for (var x = 0; x < response.tracks.items.length; x++) {
+                    // console.log("Name of song:");
+                    // console.log(x);
+                    if (song === response.tracks.items[x].name) {
+                        //console.log("Exact Match Songs:");
+                        // console.log(response.tracks.items[x]);
+                        console.log("--------------");
+                        console.log("Artist: "+response.tracks.items[x].artists[0].name);
+                        //console.log("--------------");
+                        console.log("Song: "+response.tracks.items[x].name);
+                        //console.log("--------------");
+                        console.log("Spotify Link: "+response.tracks.items[x].external_urls.spotify);
+                        //console.log("--------------");
+                        console.log("Album: "+response.tracks.items[x].album.name);
+                        console.log("--------------");
+                        //console.log(x);
+                        //console.log("--------------");
+                    }
+                }
+
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+
         break;
 
     case "movie-this":
